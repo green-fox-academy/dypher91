a0371eb6e9a1a6e69e6031f27e60ab3e84040379
Pavel Cicu <cicupa@gmail.com>
2018-11-07 11:15:36 +0100

    workshop set up

---
+++ week-07/day-3/workshop/src/main/java/com/greenfox/dypher/day3/Day3Application.java
@@ -0,0 +1,12 @@
+package com.greenfox.dypher.day3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3Application.class, args);
+    }
+}
