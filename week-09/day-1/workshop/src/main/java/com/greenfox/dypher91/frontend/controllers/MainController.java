e513ab1f6cfba73a93cbcb09e0f2b62c4ca28571
Pavel Cicu <cicupa@gmail.com>
2018-11-19 11:10:37 +0100

    main controller

---
+++ week-09/day-1/workshop/src/main/java/com/greenfox/dypher91/frontend/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.greenfox.dypher91.frontend.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/index")
+public class MainController {
+
+    
+
+}
